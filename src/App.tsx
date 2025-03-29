fb88088330a768f2e949479c3024804c054d042c
João Victor Silva <joaovictordes02@gmail.com>
2025-03-29 02:46:39 -0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+// import Heading from "./components/Heading";
+import { Container } from "./components/Container";
+import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import Heading from "./components/Heading";
+import CountDown from "./components/CountDown";
 
 function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
