ca6309c1002c92011ecad0bc361fba5e6a486833
João Victor Silva <joaovictordes02@gmail.com>
2025-03-30 01:41:35 -0300

    add component -input

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Container } from "./components/Container";
 import Logo from "./components/Logo";
 import Menu from "./components/Menu";
-
+import { Input } from "./components/Input";
 import "./styles/theme.css";
 import "./styles/global.css";
 import CountDown from "./components/CountDown";
@@ -19,6 +19,29 @@ function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <Input
+              labelText="task"
+              id="Input"
+              type="text"
+              placeholder="Digite algo..."
+            />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 </p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
